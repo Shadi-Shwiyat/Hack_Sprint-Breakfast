8c3f4847d167dba1d569fedf6bd68f6eb3ba3451
Nomad-Rob <rfarley89@gmail.com>
2023-08-17 14:21:04 -0500

    updates to meal list

--- meal_list.py
+++ meal_list.py
@@ -1,26 +1,25 @@
 class BreakfastMeal:
-    def __init__(self, name, ingredients, meal_picture, meal_sound):
+    def __init__(self, name, ingredients, meal_picture):
         self.name = name
         self.ingredients = ingredients = ingredients
         self.meal_picture = meal_picture
-        self.meal_sound = meal_sound
     
 
-
-#Breakfast Meals to be used per level
-    
+# Breakfast Meals to be used per level
 breakfasts = [
-    BreakfastMeal("Eggs and Sasauge",["Eggs", "Sasuage"], meal_picture, meal_sound)
-    BreakfastMeal("Bagel and Cream Cheese",["Bagel", "Cream Cheese"], meal_picture, meal_sound)
-    BreakfastMeal("Quiche",["Eggs", "Cheese", "Milk", "Vegetables", "Pie Crust"], meal_picture, meal_sound)
-#Breakfast Meals to be used per level (More Difficult)
-    BreakfastMeal("Quiche",["Eggs", "Cheese", "Milk", "Vegetables", "Water", "Flour", "Salt", "Butter" ], meal_picture, meal_sound)
-    BreakfastMeal("Bacon_Pancakes",["Pancake Batter", "Bacon", "Maple Syrup", "Butter"], meal_picture, meal_sound)
-    BreakfastMeal("Breakfast_Burrito",["Tortilla", "Scrambled Eggs", "Sausage", "Cheese", "Salsa"], meal_picture, meal_sound)
-    BreakfastMeal("Chocolate_Chip_Waffles", ["Milk", "Eggs", "Flour", "Baking Powder", "Sugar", "Butter", "Chocolate Chips", "Syrup"], meal_picture, meal_sound)
-    BreakfastMeal("Full_English_Breakfast",["Eggs", "Bacon", "Toast", "Jam" "Hash Browns", "Sausage", "Baked Beans", "Tomato", "Mushrooms"], meal_picture, meal_sound)
-    BreakfastMeal("Chinese_Congee", ["Rice", "Water", "Fried Egg", "Ginger", "Green Onion", "Soy Sauce"], meal_picture, meal_sound)
-    BreakfastMeal("Japanese_Breakfast", ["Rice", "Miso Soup", "Grilled Fish", "Pickled Vegetables", "Fermented Soybean", "Rolled Omelette", "Seaweed"], meal_picture, meal_sound)]
+    BreakfastMeal("Eggs_and_Sasauge",["Eggs", "Sasuage"], "Eggs_and_Sasuage.jpg")
+    BreakfastMeal("Bagel_and_Cream_Cheese",["Bagel", "Cream Cheese"], "Bagel_and_Cream_Cheese.jpg")
+    BreakfastMeal("Quiche",["Eggs", "Cheese", "Milk", "Vegetables", "Pie Crust"], "Quiche.jpg")
+    # Breakfast Meals to be used per level (More Difficult)
+    BreakfastMeal("Quiche",["Eggs", "Cheese", "Milk", "Vegetables", "Water", "Flour", "Salt", "Butter" ], "Quiche.jpg")
+    BreakfastMeal("Bacon_Pancakes",["Pancake Batter", "Bacon", "Maple Syrup", "Butter"], "Bacon_Pancakes.jpg")
+    BreakfastMeal("Breakfast_Burrito",["Tortilla", "Scrambled Eggs", "Sausage", "Cheese", "Salsa"], "Breakfast_Burrito.jpg")
+    BreakfastMeal("Chocolate_Chip_Waffles", ["Milk", "Eggs", "Flour", "Baking Powder", "Sugar", "Butter", "Chocolate Chips", "Syrup"], "Chocolate_Chip_Waffles.jpg")
+    BreakfastMeal("Full_English_Breakfast",["Eggs", "Bacon", "Toast", "Jam" "Hash Browns", "Sausage", "Baked Beans", "Tomato", "Mushrooms"], "Full_English_Breakfast.jpg")
+    BreakfastMeal("Chinese_Congee", ["Rice", "Water", "Fried Egg", "Ginger", "Green Onion", "Soy Sauce"], "Chinese_Congee.jpg")
+    BreakfastMeal("Japanese_Breakfast", ["Rice", "Miso Soup", "Grilled Fish", "Pickled Vegetables", "Fermented Soybean", "Rolled Omelette", "Seaweed"], "Japanese_Breakfast.jpg")
+    ]
+
 
 # List of right and wrong ingredients to be used per level
 Level1Ingredients = ["Eggs", "Sasuage", "Toast", "Bacon"]
