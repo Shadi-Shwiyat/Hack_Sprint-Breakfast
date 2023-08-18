edfdb8130868ff28bf3155fd01ef61ceca491cee
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-18 13:39:33 -0500

    Added all images currently needed to images folder and import them into main game

--- title.py
+++ title.py
@@ -19,10 +19,25 @@ background = pygame.transform.scale(background, (1480, 900))
 # Images to display over background
 table = pygame.image.load("images/table.png")
 table = pygame.transform.scale(table, (1200, 530))
-texbox_table = pygame.image.load("images/textbox_table.png")
-texbox_table = pygame.transform.scale(texbox_table, (1200, 530))
+texbox = pygame.image.load("images/textbox.png")
+texbox = pygame.transform.scale(texbox, (1200, 260))
 plate = pygame.image.load("images/plate.png")
 plate = pygame.transform.scale(plate, (50, 50))
+# Chef Wes Poses
+standard_pose = pygame.image.load("images/standard_pose.png")
+standard_pose = pygame.transform.scale(standard_pose, (530, 690))
+disgusted = pygame.image.load("images/disgusted.png")
+disgusted = pygame.transform.scale(disgusted, (760, 900))
+almost = pygame.image.load("images/almost.png")
+almost = pygame.transform.scale(almost, (600, 760))
+delicioso = pygame.image.load("images/delicioso.png")
+delicioso = pygame.transform.scale(delicioso, (690, 830))
+nope = pygame.image.load("images/nope.png")
+nope = pygame.transform.scale(nope, (600, 700))
+puke = pygame.image.load("images/puke.png")
+puke = pygame.transform.scale(puke, (600, 700))
+cook_it = pygame.image.load("images/cook_it.png")
+cook_it = pygame.transform.scale(cook_it, (100, 100))
 
 # Game loop
 clock = pygame.time.Clock() # Creating a clock object
@@ -36,8 +51,15 @@ while run:
 
     # Blit Background and Assets to the screen
     screen.blit(background, (-110, -50))
+    #screen.blit(standard_pose, (530, 45))
+    #screen.blit(disgusted, (530, 45))
+    #screen.blit(almost, (530, 15))
+    #screen.blit(delicioso, (560, 15))
+    #screen.blit(nope, (560, 15))
+    #screen.blit(puke, (560, 20))
     screen.blit(table, (40, 300))
-    #screen.blit(texbox_table, (40, 200))
+    screen.blit(texbox, (40, 460))
+    screen.blit(cook_it, (1000, 536))
 
     # Update the display
     pygame.display.flip()
