d1f973ff63c28ca1f090346da04e7ce53b9ec444
Nomad-Rob <rfarley89@gmail.com>
2023-08-19 10:07:19 -0500

    Adding a good working text animation

--- testing_text_animation.py
+++ testing_text_animation.py
@@ -2,12 +2,13 @@ import pygame
 import time
 
 class TextAnimation:
-    def __init__(self, sentences, x, y, font, color, delay, erase_delay):
+    def __init__(self, sentences, x, y, font, color, width, delay, erase_delay):
         self.sentences = sentences
         self.x = x
         self.y = y
         self.font = font
         self.color = color
+        self.width = width
         self.delay = delay
         self.erase_delay = erase_delay
         self.finished = False
@@ -26,18 +27,38 @@ class TextAnimation:
                     self.start_time = current_time
                 else:
                     if current_time - self.start_time >= self.erase_delay:
-                        self.current_sentence_index += 1
-                        self.current_index = 0
-                        self.display_text = ""
-                        self.start_time = current_time
-
-                        if self.current_sentence_index >= len(self.sentences):
+                        if self.current_sentence_index < len(self.sentences) - 1:
+                            self.current_sentence_index += 1
+                            self.current_index = 0
+                            self.display_text = ""
+                            self.start_time = current_time
+                        else:
                             self.finished = True
     
     def draw(self, screen):
-        text_surface = self.font.render(self.display_text, True, self.color)
-        text_rect = text_surface.get_rect(topleft=(self.x, self.y))
-        screen.blit(text_surface, text_rect)
+        words = self.display_text.split()
+        lines = []
+        current_line = ""
+        
+        for word in words:
+            test_line = f"{current_line} {word}".strip()
+            test_width, _ = self.font.size(test_line)
+            
+            if test_width <= self.width:
+                current_line = test_line
+            else:
+                lines.append(current_line)
+                current_line = word
+        
+        lines.append(current_line)
+        
+        y_offset = 0
+        for line in lines:
+            text_surface = self.font.render(line, True, self.color)
+            text_rect = text_surface.get_rect(topleft=(self.x, self.y + y_offset))
+            text_rect.width = self.width
+            screen.blit(text_surface, text_rect)
+            y_offset += text_rect.height  # Adjust y offset for the next line
 
 # Initialize Pygame
 pygame.init()
@@ -59,16 +80,17 @@ level1_sentences = [
     "This is my kitchen where I'd like to cook some breakfast with you.",
     "Lets get the ingredients ready.",
     "We will be making Eggs and Sasuage.",
+    "Pick the right ingredients below for Eggs and Sasuage."
 ]
 
 level2_sentences = [
-    "Congratulations! You've completed Level 1.",
-    "Now let's dive into more advanced recipes in Level 2."
+    "Awesome! You've completed Level 1.",
+    "BLAH BLAH BLAH"
 ]
 
 # Create TextAnimation instances for each level
-level1_animation = TextAnimation(level1_sentences, 100, 100, FONT, BLACK, 0.1, 3)
-level2_animation = TextAnimation(level2_sentences, 100, 100, FONT, BLACK, 0.1, 3)
+level1_animation = TextAnimation(level1_sentences, 275, 100, FONT, BLACK, 250, 0.1, 3)
+level2_animation = TextAnimation(level2_sentences, 275, 100, FONT, BLACK, 250, 0.1, 3)
 
 # Set the active animation (change this based on the level)
 active_animation = level1_animation
@@ -87,6 +109,6 @@ while running:
     active_animation.draw(screen)
 
     pygame.display.flip()
-    pygame.time.delay(10)
+    pygame.time.delay(5)
 
 pygame.quit()
