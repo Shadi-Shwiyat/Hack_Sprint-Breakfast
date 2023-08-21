475bb9b644962bd621d37c52375cc029625c7abc
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-21 12:10:52 -0500

    Implemented functioning start menu

--- game.py
+++ game.py
@@ -15,19 +15,26 @@ screen = pygame.display.set_mode((1280, 720))
 # Setting title of window
 pygame.display.set_caption("Rise and Dine: Wes's Cozy Kitchen")
 
-# Load font
-font_path = "./Minecraft.ttf"
-font = pygame.font.Font(None, 30)
+# Load font ### Trying to get to work ###
+#font_path = "./Minecraft.ttf"
+#font = pygame.font.Font(None, 30)
 
-# Define start cooking and continue button
+# Define start menu, cooking and continue buttons
 from button import Button
 start_cooking_button = Button("Start Cooking!", (913, 560), font_size=30, size=(200, 50), hover_size=(200, 40))
 start_cooking_button.button_color = (0, 0, 0, 0)
 progress = Button("Continue", (800, 480), font_size=36, size=(200, 50), hover_size=(200, 40))
 progress.button_color = (0, 0, 0, 0)
+start_button = Button("Start", (476, 439), font_size=76, size=(260, 60), hover_size=(260, 50))
+start_button.button_color = (0, 0, 0, 0)
+quit_button = Button("Exit", (476, 576), font_size=76, size=(260, 60), hover_size=(260, 50))
+quit_button.button_color = (0, 0, 0, 0)
 
 # Load level setup from function
-current_level = 1
+current_level = 0
+if current_level == 0:
+    menu = pygame.image.load("images/start_menu.png")
+    menu = pygame.transform.scale(menu, (500, 720))
 level_data = current_level_setup(current_level)
 current_ingredients = level_data["current_ingredients"]
 right_ingredients = level_data["right_ingredients"]
@@ -82,88 +89,121 @@ while run:
     # Things to clear each loop iteration
 
     # Update the text animation
-    level_text.update()
+    if current_level >= 1:
+        level_text.update()
 
-    # Event to quit loop when user hits X
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        # Event to quit loop when user hits X
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+                
+            if level_text.finished:
+                # Event for ingredient button clicks
+                for button in buttons:
+                    button.handle_events(event)
+                    # Adding selected buttons to list
+                    if button.selected and button.text not in selected_ingredients:
+                        selected_ingredients.append(button.text)
+                        print(selected_ingredients)
+                    elif button.selected == False and button.text in selected_ingredients:
+                        selected_ingredients.remove(button.text)
+                        print(selected_ingredients)
+
+                # Event for start cooking button
+                start_cooking_button.handle_events(event)
+                if start_cooking_button.clicked:
+                    start_cooking_button.clicked = False
+                    start_cooking_button.selected = False
+                    progress.clicked = False
+                    print("Start Cooking button clicked!")
+
+                    # Compare user-selected ingredients with correct ingredients
+                    if sorted(selected_ingredients) == sorted(right_ingredients):
+                        print("Ingredients match!")
+                        level_success = True
+                        screen.blit(background, (-110, -50))
+                        screen.blit(delicioso, (630, 15))
+                        screen.blit(table, (40, 300))
+                        screen.blit(chatbox, (273, 76))
+                        # Blit the meal_picture on the screen
+                        screen.blit(plate, (300, 430))
+                        screen.blit(current_meal_picture, (326, 430))
+                    else:
+                        ingredients_compared = True
+                        image_flip = not image_flip
+                        print("Ingredients do not match..")
+
+                # Continue button to go to next level
+                if level_success:
+                    progress.handle_events(event)
+                    screen.blit(cook_it, (819, 456))
+                    if progress.clicked:
+                        print("progress clicked")
+                        progress.clicked = False
+                        progress.selected = False
+                        if current_level <= max_levels:
+                            current_level += 1
+                            level_data = current_level_setup(current_level)
+                            current_ingredients = level_data["current_ingredients"]
+                            right_ingredients = level_data["right_ingredients"]
+                            level_text = level_data["level_text"]
+                            current_meal_picture = level_data["current_meal_picture"]
+                            buttons = level_data["buttons"]
+                            ingredients_compared = False
+                            level_text.finished = False
+                            level_success = False
+
+                    selected_ingredients = [] # Clear the user-selected ingredients list for the next level
+    else:
+        # Event to quit loop when user hits X
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        # Event listeners for start menu
+        start_button.handle_events(event)
+        if start_button.clicked:
+            start_button.clicked = False
+            start_button.selected = False
+            current_level += 1
+            level_data = current_level_setup(current_level)
+            current_ingredients = level_data["current_ingredients"]
+            right_ingredients = level_data["right_ingredients"]
+            level_text = level_data["level_text"]
+            current_meal_picture = level_data["current_meal_picture"]
+            buttons = level_data["buttons"]
+        quit_button.handle_events(event)
+        if quit_button.clicked:
             pygame.quit()
             exit()
-            
-        if level_text.finished:
-            # Event for ingredient button clicks
-            for button in buttons:
-                button.handle_events(event)
-                # Adding selected buttons to list
-                if button.selected and button.text not in selected_ingredients:
-                    selected_ingredients.append(button.text)
-                    print(selected_ingredients)
-                elif button.selected == False and button.text in selected_ingredients:
-                    selected_ingredients.remove(button.text)
-                    print(selected_ingredients)
+                    
 
-            # Event for start cooking button
-            start_cooking_button.handle_events(event)
-            if start_cooking_button.clicked:
-                start_cooking_button.clicked = False
-                start_cooking_button.selected = False
-                progress.clicked = False
-                print("Start Cooking button clicked!")
 
-                # Compare user-selected ingredients with correct ingredients
-                if sorted(selected_ingredients) == sorted(right_ingredients):
-                    print("Ingredients match!")
-                    level_success = True
-                    screen.blit(background, (-110, -50))
-                    screen.blit(delicioso, (630, 15))
-                    screen.blit(table, (40, 300))
-                    screen.blit(chatbox, (273, 76))
-                    # Blit the meal_picture on the screen
-                    screen.blit(plate, (300, 430))
-                    screen.blit(current_meal_picture, (326, 430))
-                else:
-                    ingredients_compared = True
-                    image_flip = not image_flip
-                    print("Ingredients do not match..")
 
-            # Continue button to go to next level
-            if level_success:
-                progress.handle_events(event)
-                screen.blit(cook_it, (819, 456))
-                if progress.clicked:
-                    print("progress clicked")
-                    progress.clicked = False
-                    progress.selected = False
-                    if current_level <= max_levels:
-                        current_level += 1
-                        level_data = current_level_setup(current_level)
-                        current_ingredients = level_data["current_ingredients"]
-                        right_ingredients = level_data["right_ingredients"]
-                        level_text = level_data["level_text"]
-                        current_meal_picture = level_data["current_meal_picture"]
-                        buttons = level_data["buttons"]
-                        ingredients_compared = False
-                        level_text.finished = False
-                        level_success = False
 
-                selected_ingredients = [] # Clear the user-selected ingredients list for the next level
 
     # Blit Background and Assets to the screen
     #screen.blit(background, (-110, -50))
 
     # Blit pose based on current player status
-    if level_success == False:
+    if current_level == 0:
         screen.blit(background, (-110, -50))
-        if ingredients_compared == False:
-            screen.blit(standard_pose, (530, 45))
-        elif image_flip:
-            screen.blit(disgusted, (530, 45))
-        else:
-            screen.blit(puke, (560, 20))
-        screen.blit(chatbox, (273, 76))
-        screen.blit(table, (40, 300))
-        if ingredients_compared:
-            screen.blit(dubious, (326, 190))
+        screen.blit(menu, (360, 0))
+    else:
+        if level_success == False:
+            screen.blit(background, (-110, -50))
+            if ingredients_compared == False:
+                screen.blit(standard_pose, (530, 45))
+            elif image_flip:
+                screen.blit(disgusted, (530, 45))
+            else:
+                screen.blit(puke, (560, 20))
+            screen.blit(chatbox, (273, 76))
+            screen.blit(table, (40, 300))
+            if ingredients_compared:
+                screen.blit(dubious, (326, 190))
     #screen.blit(almost, (530, 15))
     #screen.blit(nope, (560, 15))
 
@@ -176,6 +216,9 @@ while run:
     level_text.draw(screen) 
 
     # Draw the buttons
+    if current_level == 0:
+        start_button.draw()
+        quit_button.draw()
     if level_text.finished and level_success == False:  
         for button in buttons:
             button.draw()
